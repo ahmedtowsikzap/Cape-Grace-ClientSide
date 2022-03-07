2cc470c0f015098317ef59d3b7330b0d79d2e8f8
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-03-08 00:57:57 +0600

    Added Topnotch or hero section

--- src/Components/Home/Home/Home.jsx
+++ src/Components/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Headers from '../../Shared/Navbar/Headers';
 import Topbar from '../../Shared/Topbar/Topbar';
+import TopNotch from '../TopNotch/TopNotch';
 
 const Home = () => {
     return (
         <div>
         <Headers/>
+        <TopNotch/>
         </div>
     );
 };
---
+++ src/Components/Home/TopNotch/TopNotch.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import video from "../../../Video/transition.mp4";
+import "./TopNotch.css";
+
+const TopNotch = () => {
+  return (
+    <div>
+      <video src={video} autoPlay loop muted></video>
+      <div className="text">
+        <h2>REACH!</h2>
+      </div>
+    </div>
+  );
+};
+
+export default TopNotch;
